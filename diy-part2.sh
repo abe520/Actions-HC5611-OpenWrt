d8ff18c6b140beaa2c2f6b7986ed0e9fb816c0f7
abe520 <abe520@msn.cn>
2023-04-20 02:44:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
 
-svn co https://github.com/coolsnowwolf/lede/trunk/tools/ninja tools/ninja
+#svn co https://github.com/coolsnowwolf/lede/trunk/tools/ninja tools/ninja
